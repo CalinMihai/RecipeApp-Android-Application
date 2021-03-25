bfc3e5c3ad6c1f377e0358de9712bf26c76b9fda
Calin Mihai <calinmihai.cm98@gmail.com>
2021-03-25 10:35:24 +0200

    Implemented Accelerometer, Gyroscope and Camera

---
+++ app/src/main/java/com/example/recipeappstrike3/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.recipeappstrike3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Accelerometer accelerometer;
+    private Gyroscope gyroscope;
+    private TextView x;
+    private TextView y;
+    private TextView z;
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.profileBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openProfileActivity();
+            }
+        });
+
+        x = (TextView) findViewById(R.id.x);
+
+        accelerometer = new Accelerometer(this);
+        gyroscope = new Gyroscope(this);
+
+        accelerometer.setListener(new Accelerometer.Listener() {
+            @Override
+            public void onTranslation(float tx, float ty, float tz) {
+                if(tx > 1.0f){
+                    getWindow().getDecorView().setBackgroundColor(Color.RED);
+                    x.setText(" " + tx);
+                }
+                else if(tx < -1.0f){
+                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+                    x.setText(" " + tx);
+                }
+            }
+        });
+
+        gyroscope.setListener(new Gyroscope.Listener() {
+            @Override
+            public void onRotation(float rx, float ry, float rz) {
+                if(rz > 1.0f){
+                    getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+                }
+                else if(rz < -1.0f){
+                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+                }
+            }
+        });
+    }
+
+    public void openProfileActivity() {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        accelerometer.register();
+        gyroscope.register();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        accelerometer.unregister();
+        gyroscope.unregister();
+    }
+}
